17d548f93f7db4667a277d20402123e785b660d2
Quentin Villanova <villanoq@iut1i-702-a16.u-ga.fr>
2021-11-17 16:03:58 +0100

    first commit

---
+++ Shared/DaltoPipetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaltoPipetteApp.swift
+//  Shared
+//
+//  Created by Quentin Villanova on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DaltoPipetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
